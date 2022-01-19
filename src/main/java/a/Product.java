b9f799117d59dfab59ec1212a54b52a7256ed390
Debajyoti Bhattacharjee <debajyoti0007@gmail.com>
2022-01-19 21:56:09 +0530

    Update Product.java

--- src/main/java/a/Product.java
+++ src/main/java/a/Product.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class Product {
 	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+        @GeneratedValue(strategy = GenerationType.AUTO)
 	private Long productID;
 	private String productName;
 	private String productManufacturer;
